8d9d55230a9aebc59ddb5f462edaec25d90bbd09
Archit <jarchit29@gmail.com>
2024-05-19 00:14:02 +0530

    Added home page , manipulared text, added social media handels and made resume downloadable

--- src/app/Components/NavBar.js
+++ src/app/Components/NavBar.js
@@ -11,12 +11,13 @@ const NavBar = () => {
                         <img src="/feather.svg.png" class="h-12" alt="welcome" />
                         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Archit jain</span>
                     </a>
+                    
                     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                        <button class="Btn">
+                        <a class="Btn" href="/cv.pdf" download="cv">
                             <svg class="svgIcon" viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path></svg>
                             <span class="icon2"></span>
                             <span class="tooltip">Resume</span>
-                        </button>
+                        </a>
                         <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                             <span class="sr-only">Open main menu</span>
                             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
--- src/app/Pages/HomePage.js
+++ src/app/Pages/HomePage.js
@@ -1,8 +1,42 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import Button from '../Components/Button'
 import NavBar from '../Components/NavBar'
+import moment from 'moment/moment'
+import Footer from '../Components/Footer';
+import SocialHandels from '../Components/SocialHandels';
+
+
 
 const HomePage = () => {
+
+
+    const currentDate = moment();
+
+
+    // Create a start date (arbitrary, since we're only interested in the duration)
+    const startDate = moment("2021-05-01");
+
+    const totalDays = currentDate.diff(startDate, 'days')
+
+    // Add the total number of days to the start date to get the target date
+    const targetDate = startDate.clone().add(totalDays, 'days');
+
+    // Calculate the difference in years, months, and days
+    const years = targetDate.diff(startDate, 'years');
+    startDate.add(years, 'years');
+
+    const months = targetDate.diff(startDate, 'months');
+    startDate.add(months, 'months');
+
+    const days = targetDate.diff(startDate, 'days');
+
+    // Create the difference string
+    const difference = `${years} years, ${months} months, ${days} days`;
+
+
+
     return (
         <div>
 
@@ -11,17 +45,63 @@ const HomePage = () => {
 
             </div>
 
+            <div class="relative h-3 w-3 ...">
+                <div class="absolute inset-y-0 left-0 w-16 ...">
+                    <SocialHandels />
+                </div>
+            </div>
+
+
+
             <div className='text-center'>
 
+                <span className='heading-primary text-highlighter'>
 
-                HI I am archit jain
+                    Hi, I'm
+
+                </span>
+
+                <span className='heading-primary text-highlighters mx-3'>
+                    Archit Jain
+                </span>
+
+            </div>
+
+            <div className='flex justify-center'>
+
+
+                <div className='w-3/4 p-primary text-tertiory'>
+
+                    <div>
+                        <span className='mx-3'>
+
+                            A tech-savvy professional with
+                        </span>
+
+                        <span className='text-highlighter font-bold'>
+                            {difference}
+                        </span>
+
+                        <span className='mx-3'>
+
+                            of industry experience. I specialize in bridging the gap between business and technology as an Expert Tech and Functional Business Analyst, Full Stack Developer, and Hybrid Mobile App Innovator
+                        </span>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div className='text-center'>
 
                 <div className='m-12'>
                     <Button buttonLabel='Know About Me' />
                 </div>
-
             </div>
 
+            <Footer />
 
         </div>
 
